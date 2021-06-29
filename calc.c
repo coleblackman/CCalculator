85b9a46c4669f832f3832a44f2a0d641a27f5717
Cole Blackman <csbws@virginia.edu>
2021-06-28 22:40:45 -0400

    Implemented all functionality

--- calc.c
+++ calc.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+<<<<<<< HEAD
+=======
+#include <stdlib.h>
+>>>>>>> Implemented all functionality
 #include <string.h>
 
 #define BUFFERLEN 100 
 
+<<<<<<< HEAD
+=======
+long int powe (int base, int exponent);
+
+>>>>>>> Implemented all functionality
 int main(void)    
 {
     char expression [BUFFERLEN];
@@ -30,7 +39,51 @@ int main(void)
     printf("\nSecond Expression: %s", thirdPart);
 
 
+<<<<<<< HEAD
     printf("Press <return>\n");
     getc(stdin);
     return 0;
 }
+=======
+    // Convert to appropriate datatype
+    
+    char *end;
+    long int firstArg = strtol(firstPart, &end, 10);
+    long int secondArg = strtol(thirdPart, &end, 10);
+
+
+    switch (secondPart[0]) {
+      case '+':
+	printf("\nAddition: %3ld", firstArg+secondArg);
+	break;
+      case '-':
+	printf("\nSubtraction: %3ld", firstArg-secondArg);
+	break;
+      case '*':
+	printf("\nMultiplication: %3ld", firstArg*secondArg);
+	break;
+      case '/':
+	printf("\nDivision: %3ld", firstArg/secondArg);
+	break;
+      case '^':
+       	printf("\nPower: %3ld", powe(firstArg, secondArg));
+	break;
+    }
+
+    printf("\nPress <return>\n");
+    getc(stdin);
+    return 0;
+
+}
+
+long int powe (int base, int exponent) 
+{
+    long int ret = 1;
+    for(int i = 0; i < exponent; i++) 
+    {
+	ret = ret * base;
+    }
+    return ret;
+}
+
+>>>>>>> Implemented all functionality
